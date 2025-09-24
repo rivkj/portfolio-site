2ce17295c8199f973f2cdd99f3742c5973b696f0
rivkj <rivaskj07@gmail.com>
2025-09-24 10:33:58 -0400

    Added project 2 to design section and edited js to expand project images

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// Grab all images inside project galleries
+document.querySelectorAll(".project-gallery img").forEach((img) => {
+  img.addEventListener("click", () => {
+    openLightbox(img);
+  });
+});
+
+function openLightbox(img) {
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+  lightbox.style.display = "flex";
+  lightboxImg.src = img.src;
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
